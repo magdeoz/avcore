1506cd1bb5430379a7ac142a3bb7274105c71ae8
lenarox <lenarox@ubuntu.(none)>
2013-07-22 09:13:27 -0700

    more stuff added & built-in guide for devs

--- avcore.sh
+++ avcore.sh
@@ -1,5 +1,5 @@
 
-#ONLY suitable for systems which contains latest versions of busybox.(You may still edit BAG2.0 for better compatibility.)
+#ONLY suitable for systems which contains latest versions of busybox.(You can still edit BAG2.0 for compatibility.)
 
 #Copyright (C) 2013  LENAROX@xda
 #
@@ -17,64 +17,213 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 set +e
-#Debugger
+#Un-comment out the following line to enable debugging.
 #set -x
 
 #BAG2.0 options
+#You can type in any commands you would want it to check.
+#It will start by checking from cmd1, and its limited up to cmd199.
 cmd1=renice
 cmd2=tr
 cmd3=ionice
-cmd=3
 
-#Busybox Applet Generator 2.0
+#Busybox Applet Generator(BAG) 2.0
+#This might not be compatible with some other multi-call binaries.
 if [ ! "$(busybox --list)" ]; then
-echo "busybox: not found, damnit."
-return 1
+	echo "Failed to locate busybox!"
+	return 1
 else
-busyboxloc=$(dirname $(which busybox))
-n=0
-for i in $(echo $PATH | sed 's/:/ /g'); do
-n=$(($n+1))
-export slot$n=$i
-if [ "$i" == "$busyboxloc" ]; then
-busyboxenv=slot$n
-fi
-done
-if [ "$busyboxenv" != slot1 ]; then
-export PATH=$(echo -n $busyboxloc
-for i in $(seq -s ' $slot' 0 $n | sed 's/^0//'); do
-v=$(eval echo $i)
-if [ "$v" != "$busyboxloc" ]; then
-echo -n ":$v"
-fi
-done)
-fi
-#You might not be able to run this with other multi-call binaries.
-if [ "$cmd" -lt 0 ]; then
-cmd=0
-fi
-for i in $(seq -s ' $cmd' 0 $cmd | sed 's/^0//'); do
-v=$(eval echo $i)
-if [ ! "$(busybox --list | grep $v)" ]; then
-echo "required applets are not found, or missing!"
-return 1
-fi
-n=0
-if [ -e "$busyboxloc"/"$v" ]; then
-n=$(($n+1))
-fi
-if [ "$n" -eq 0 ]; then
-alias $i="busybox $i"
-fi
-done
+	busyboxloc=$(dirname $(which busybox))
+	n=0
+	for i in $(echo $PATH | sed 's/:/ /g'); do
+		n=$(($n+1))
+		export slot$n=$i
+		if [ "$i" == "$busyboxloc" ]; then
+			busyboxenv=slot$n
+		fi
+	done
+	if [ "$busyboxenv" != slot1 ]; then
+		export PATH=$(echo -n $busyboxloc
+		for i in $(seq -s ' $slot' 0 $n | sed 's/^0//'); do
+			v=$(eval echo $i)
+			if [ "$v" != "$busyboxloc" ]; then
+				echo -n ":$v"
+			fi
+		done)
+	fi
+	if [ "$cmd" -lt 0 ]; then
+		cmd=0
+	fi
+	for i in $(seq -s ' $cmd' 0 199 | sed 's/^0//'); do
+		v=$(eval echo $i)
+		if [ "$v" ]; then
+			if [ ! "$(busybox --list | grep $v)" ]; then
+				echo "Required applets are missing!"
+				return 1
+			fi
+			n=0
+			if [ -e "$busyboxloc"/"$v" ]; then
+				n=$(($n+1))
+			fi
+			if [ "$n" -eq 0 ]; then
+				alias $i="busybox $i"
+			fi
+		else
+			break
+		fi
+	done
 fi 2>/dev/null
 
-#Help must be universal to all parties.
+#Check Superuser.
+if [ "$(id -u)" != 0 ] && [ "$(id -u)" != root ]; then
+	echo "Permission denied, are you root?"
+	return 1
+fi
+
+################################USER EDITABLE AREA################################
+
+#Put your engine stuffs here.
+Roll_Up()
+{
+	if [ "$?" -eq 0 ]; then
+		if [ "$opt_p" -eq 1 ]; then
+			echo "priority: $opt_p_val" 
+		fi
+		if [ "$opt_x" -eq 1 ]; then
+			echo test confirmed
+		fi
+		if [ "$opt_h" -eq 1 ]; then
+			Usage
+		fi
+	else
+		Usage
+	fi
+}
+
+#Put your extra options for your engine here.
+Magic_Parser()
+{
+	opt_p=0
+	opt_p_val=0
+	opt_x=0
+	opt_h=0
+	while [ "$1" ]; do
+		case $1 in
+			-p* | --priority* )
+				if [ "$(echo $1 | grep -w "^-p.*")" ]; then
+					mode="-p"
+				else
+					mode="--priority"
+				fi
+				if [ "$opt_p" -gt 0 ]; then
+					echo "$mode": same operation not permitted
+					return 1
+				fi
+				opt_p=$(($opt_p+1))
+				if [ ! "$(echo $1 | sed 's/^'"$mode"'//')" ]; then
+					if [ "$2" ]; then
+						opt_p_val=$2
+					else
+						echo "$mode": requires a value
+						return 1
+					fi
+					if [ "$(echo $opt_p_val | grep "^-")" ]; then
+						echo "$mode": requires a value
+						return 1
+					else
+						if [ "$(echo $opt_p_val | tr -d "0-9")" ]; then
+							echo "$mode": requires an integer number as a value
+							return 1
+						fi
+					fi
+					shift
+				else
+					if [ "$(echo $1 | sed 's/^'"$mode"'//' | tr -d "0-9")" ]; then
+						echo "$mode": requires an integer number as a value
+						return 1
+					fi
+					opt_p_val=$(echo $1 | sed 's/^'"$mode"'//')
+				fi
+			;;
+			* )
+				if [ ! "$(echo $1 | sed 's/^-//')" ]; then
+					echo "$1": invalid argument
+					return 1
+				fi
+				n=0
+				for i in $(echo $1 | sed 's/.\{1\}/& /g'); do
+					if [ "$i" != "-" ]; then
+						break
+					fi
+					n=$(($n+1))
+				done
+				if [ "$n" -eq 1 ]; then
+					if [ ! "$(echo $1 | sed 's/^-//' | tr -d "0-9")" ]; then
+						if [ "$opt_p" -gt 0 ]; then
+							if [ "$mode" ]; then
+								echo "$mode": same operation not permitted
+							else
+								echo "-p": same operation not permitted
+							fi
+							return 1
+						fi
+						opt_p=$(($opt_p+1))
+						opt_p_val=$(echo $1 | sed 's/^-//')
+					else
+						for i in $(echo $1 | sed 's/^-//' | sed 's/.\{1\}/& /g'); do
+							if [ "$i" == x ]; then
+								if [ "$opt_x" -gt 0 ]; then
+									echo "-x": same operation not permitted
+									return 1
+								fi
+								opt_x=$(($opt_x+1))
+							elif [ "$i" == h ]; then
+								if [ "$opt_h" -gt 0 ]; then
+									echo "-h": same operation not permitted
+									return 1
+								fi
+								opt_h=$(($opt_h+1))
+							else
+								echo "$1": invalid argument
+								return 1
+							fi
+						done
+					fi
+				elif [ "$n" -eq 2 ]; then
+					for i in $(echo $1 | sed 's/^--//'); do
+						if [ "$i" == exit ]; then
+							if [ "$opt_x" -gt 0 ]; then
+								echo "--exit": same operation not permitted
+								return 1
+							fi
+							opt_x=$(($opt_x+1))
+						elif [ "$i" == help ]; then
+							if [ "$opt_h" -gt 0 ]; then
+								echo "--help": same operation not permitted
+								return 1
+							fi
+							opt_h=$(($opt_h+1))
+						else
+							echo "$1": invalid argument
+							return 1
+						fi
+					done
+				else
+					echo "$1": invalid argument
+					return 1
+				fi
+			;;
+		esac
+		shift
+	done
+}
+
+#Customize Usage for your own needs.
 Usage()
 {
-for i in $(echo $0 | sed 's/\// /g'); do
-done
-echo "Usage: $i -hx -p [VALUE]
+	for i in $(echo $0 | sed 's/\// /g'); do
+	done
+	echo "Usage: $i -hx -p [VALUE]
 	-p turns on options to set priority manually. defaulted to 19.
 	-x ends the process already running in background.
 	-h views this message.
@@ -84,155 +233,11 @@ echo "Usage: $i -hx -p [VALUE]
 "
 }
 
-#Mad Logic Checker 1.0
-Magic_Check()
-{
-opt_p=0
-opt_p_val=0
-opt_x=0
-opt_h=0
-while [ "$1" ]; do
-case $1 in
--p* | --priority* )
-if [ "$(echo $1 | grep "^-p")" ]; then
-mode="-p"
-else
-mode="--priority"
-fi
-if [ "$opt_p" -gt 0 ]; then
-echo "$mode": same operation not permitted
-Usage
-return 1
-fi
-opt_p=$(($opt_p+1))
-if [ ! "$(echo $1 | sed 's/^'"$mode"'//')" ]; then
-if [ "$2" ]; then
-opt_p_val=$2
-else
-echo "$mode": requires a value
-Usage
-return 1
-fi
-if [ "$(echo $opt_p_val | grep "^-")" ]; then
-echo "$mode": requires a value
-Usage
-return 1
-else
-if [ "$(echo $opt_p_val | tr -d "0-9")" ]; then
-echo "$mode": requires an integer number as a value
-Usage
-return 1
-fi
-fi
-shift
-else
-if [ "$(echo $1 | sed 's/^'"$mode"'//' | tr -d "0-9")" ]; then
-echo "$mode": requires an integer number as a value
-Usage
-return 1
-fi
-opt_p_val=$(echo $1 | sed 's/^'"$mode"'//')
-fi
-;;
-* )
-if [ ! "$(echo $1 | sed 's/^-//')" ]; then
-echo "$1": invalid argument
-Usage
-return 1
-fi
-n=0
-for i in $(echo $1 | sed 's/.\{1\}/& /g'); do
-if [ "$i" != "-" ]; then
-break
-fi
-n=$(($n+1))
-done
-if [ "$n" -eq 1 ]; then
-if [ ! "$(echo $1 | sed 's/^-//' | tr -d "0-9")" ]; then
-if [ "$opt_p" -gt 0 ]; then
-if [ "$mode" ]; then
-echo "$mode": same operation not permitted
-else
-echo "-p": same operation not permitted
-fi
-Usage
-return 1
-fi
-opt_p=$(($opt_p+1))
-opt_p_val=$(echo $1 | sed 's/^-//')
-else
-for i in $(echo $1 | sed 's/^-//' | sed 's/.\{1\}/& /g'); do
-if [ "$i" == x ]; then
-if [ "$opt_x" -gt 0 ]; then
-echo "-x": same operation not permitted
-Usage
-return 1
-fi
-opt_x=$(($opt_x+1))
-elif [ "$i" == h ]; then
-if [ "$opt_h" -gt 0 ]; then
-echo "-h": same operation not permitted
-Usage
-return 1
-fi
-opt_h=$(($opt_h+1))
-else
-echo "$1": invalid argument
-Usage
-return 1
-fi
-done
-fi
-elif [ "$n" -eq 2 ]; then
-for i in $(echo $1 | sed 's/^--//'); do
-if [ "$i" == exit ]; then
-if [ "$opt_x" -gt 0 ]; then
-echo "--exit": same operation not permitted
-Usage
-return 1
-fi
-opt_x=$(($opt_x+1))
-elif [ "$i" == help ]; then
-if [ "$opt_h" -gt 0 ]; then
-echo "--help": same operation not permitted
-Usage
-return 1
-fi
-opt_h=$(($opt_h+1))
-else
-echo "$1": invalid argument
-Usage
-return 1
-fi
-done
-else
-echo "$1": invalid argument
-Usage
-return 1
-fi
-;;
-esac
-shift
-done
-}
+#And, you're good to go!
 
-#Final results
-Roll_Up()
-{
-if [ "$opt_p" -eq 1 ]; then
-echo "priority: $opt_p_val" 
-fi
-if [ "$opt_x" -eq 1 ]; then
-echo test confirmed
-fi
-if [ "$opt_h" -eq 1 ]; then
-Usage
-fi
-}
+##########IMPORTANT##########DO NOT CROSS THE LINE!!!############STUFF############
 
 #Main script
-Magic_Check $@
+Magic_Parser $@
 Roll_Up
 
-#End session
-return 0
