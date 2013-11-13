feb1dfee14a602ed259884a4769f84a1406b7ef5
lenarox <hoholee12@naver.com>
2013-11-13 05:33:32 -0800

    update

--- chklnk.sh
+++ chklnk.sh
@@ -1,4 +1,21 @@
 #chklnk
+#
+# Copyright (C) 2013  hoholee12@naver.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Changelogs:
+# alpha version
+# 0.0.1 - first release
+set +e
 if [ ! $1 ]; then
 	return 1
 fi
@@ -7,7 +24,7 @@ dir=$(dirname $file)
 base=$(basename $file)
 if [ ! -e $file ] && [ ! -d $file ]; then
 	echo "$file: not found"
-	return 1
+	return 127
 fi
 count=0
 for i in $(ls -l $dir | grep $base | head -1); do
