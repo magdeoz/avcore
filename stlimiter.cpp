1927790a93df08dae2f35c6aeee4b9a46e5165c3
hoholee12 <hoholee12@naver.com>
2015-06-18 18:16:32 +0900

    swap under one jiffy

--- stlimiter.cpp
+++ stlimiter.cpp
@@ -1,19 +1,31 @@
-#include<iostream>
-#include<unistd.h>
-#include<stdlib.h>
-#include<signal.h>
-#include<sys/types.h>
+/*
+# stlimiter.cpp
+#
+# Copyright (C) 2013-2015  hoholee12@naver.com
+#
+# May be freely distributed and modified as long as copyright
+# is retained.
+*/
+
+#include<iostream> //kill
+#include<unistd.h> //usleep, getuid
+#include<stdlib.h> //atoi
+#include<signal.h> //SIGCONT, SIGSTOP
+#include<sys/types.h> //i dont know what this contains.
 using namespace std;
 
-static const int msecs=1000;
+static const int jiffy=10000;
 static int pid;
-static int limiter;
+static double limiter;
+static int sigstopsleepr;
+static int sigcontsleepr;
+static const int enable_swap=1;
 
-void send_signal(){
-        kill(::pid, SIGSTOP);
-        usleep(msecs*limiter);
-        kill(::pid, SIGCONT);
-        usleep(msecs*10);//10 millisecs == one jiffy
+inline void send_signal(){
+	kill(::pid, SIGSTOP);
+	usleep(::sigstopsleepr);
+	kill(::pid, SIGCONT);
+	usleep(::sigcontsleepr);
 }
 
 void handler(int){
@@ -21,21 +33,32 @@ void handler(int){
 	exit(0);
 }
 
+void swap(int &a, int &b){
+	if(::enable_swap==1){
+		int tmp=a; a=b; b=tmp;
+	}
+}
+
 int main(int argc, char **argv){
 	if(!argv[1]){
 cout<<"Copyright (C) 2013-2015  hoholee12@naver.com"<<endl;
-cout<<"Usage: "<<argv[0]<<" [milliseconds] [pid]"<<endl; return 0;}
-        pid=atoi(argv[2]);
-	limiter=atoi(argv[1])-10;
-        if(getuid()){
-                cout<<"big problem."<<endl;
-                return 1;
-        }
-        if(kill(pid, 0)){
-                cout<<"second big problem."<<endl;
-                return 1;
-        }
-		signal(SIGINT, handler);
+cout<<"Usage: "<<argv[0]<<" [percentage] [pid]"<<endl; return 0;}
+	pid=atoi(argv[2]);
+	limiter=atof(argv[1]);
+	if(limiter>100.0) limiter=100.0;
+	else if(limiter<0.0) limiter=0.0;
+	if(getuid()){
+		cout<<"Permission denied, are you root?"<<endl;
+		return 1;
+	}
+	if(kill(pid, 0)){
+		cout<<"process "<<pid<<" does not exist."<<endl;
+		return 1;
+	}
+	sigstopsleepr=jiffy*limiter/100;
+	sigcontsleepr=jiffy*(100-limiter)/100;
+	swap(sigstopsleepr, sigcontsleepr);
+	signal(SIGINT, handler);
 	for(;;) send_signal();
-        return 0;
+	return 0;
 }
